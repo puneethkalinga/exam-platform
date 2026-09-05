79cadc2daaae56fdb05ffd4badb19dacdc08d1a0
saikrishna7240 <saikrishnabitla724@gmail.com>
2026-09-05 09:59:07 +0530

    Fix coding exam routes and backend deployment

--- backend/src/routes/codingExamRoutes.js
+++ backend/src/routes/codingExamRoutes.js
@@ -8,16 +8,26 @@ const {
   getCodingExamById,
   updateCodingExam,
   deleteCodingExam,
+  publishCodingExam,
 } = require("../controllers/codingExamController");
 
 const authMiddleware = require("../middleware/authMiddleware");
 
 // Admin
 router.get("/", authMiddleware, getCodingExams);
+
 router.post("/", authMiddleware, createCodingExam);
+
 router.put("/:id", authMiddleware, updateCodingExam);
+
 router.delete("/:id", authMiddleware, deleteCodingExam);
-router.put("/:id/publish", authMiddleware, publishCodingExam);
+
+router.put(
+  "/:id/publish",
+  authMiddleware,
+  publishCodingExam
+);
+
 // Public candidate exam information
 router.get("/:id", getCodingExamById);
 
